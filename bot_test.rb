f2ac34894d2fc5bd79a48653a6fab023af4a9421
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2020-10-29 23:21:09 +0900

    changed token key to env var

--- bot_test.rb
+++ bot_test.rb
@@ -1,8 +1,8 @@
 require 'discordrb'
 
 bot = Discordrb::Commands::CommandBot.new(
-token: 'NzcwNjU0NDU2MjMxNDkzNjMz.X5gt5A.eYp3BqIu-vfVcOPk7isxhphEnC4',
-client_id:770654456231493633,
+token: ENV['TOKEN'],
+client_id: ENV['CLIENT_ID'],
 prefix:'/',
 )
 
