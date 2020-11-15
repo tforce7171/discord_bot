f81f36876e5c10bcdebde352ac85cd00bd4f1bc5
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2020-11-15 17:09:02 +0900

    put api inside discord command

--- bot_test.rb
+++ bot_test.rb
@@ -8,21 +8,20 @@ client_id: ENV['CLIENT_ID'],
 prefix:'/',
 )
 
-url = "https://api.wotblitz.asia/wotb/account/achievements"
-client = HTTPClient.new
-query = {
-  'application_id' => ENV['APPLICATION_ID'],
-  'account_id' => '2010886246'
-}
-response = client.get(url, query)
-result = JSON.parse(response.body)
-
-if result["data"]
-  medalHalonen = result["data"]["2010886246"]["achievements"]["medalHalonen"]
-end
-
 bot.command :ola do |event|
- event.send_message("#{medalHalonen}")
+  url = "https://api.wotblitz.asia/wotb/account/achievements"
+  client = HTTPClient.new
+  query = {
+    'application_id' => ENV['APPLICATION_ID'],
+    'account_id' => '2010886246'
+  }
+  response = client.get(url, query)
+  result = JSON.parse(response.body)
+
+  if result["data"]
+    medalHalonen = result["data"]["2010886246"]["achievements"]["medalHalonen"]
+  end
+  event.send_message("#{medalHalonen}")
 end
 
 bot.run
