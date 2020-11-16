afd91d6a8d23504f7c5180990520526911883f7d
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2020-11-17 00:10:50 +0900

    Add files via upload

--- bot_test.rb
+++ bot_test.rb
@@ -11,39 +11,30 @@ prefix:'/',
 
 bot.command :ola do |event|
 
-  event.send_message("starting")
-
   url = "https://api.wotblitz.asia/wotb/tournaments/list/?application_id=eda85c3d6ddbb56920d3544319a4a788&fields=start_at%2Ctitle%2Ctournament_id"
   client = HTTPClient.new
   response = client.get(url)
   results = JSON.parse(response.body)
-
-  event.send_message("api parsed")
-  if results["data"]
-    event.send_message("api retrieved#{results["data"]}")
-  end
 
   results["data"].each do |result|
     today_unix = Date.today.to_time.to_i-32400
     start_at_date_unix = result["start_at"]-72000
     tournament_id = result["tournament_id"]
 
-    event.send_message("#{today_unix};#{start_at_date_unix};#{tournament_id}")
-
     if today_unix == start_at_date_unix
-      event.send_message("date match found")
+      qt_count = 0
       url = "https://api.wotblitz.asia/wotb/tournaments/stages/?application_id=eda85c3d6ddbb56920d3544319a4a788&tournament_id=#{tournament_id}"
       client = HTTPClient.new
       response = client.get(url)
       result = JSON.parse(response.body)
       if result["meta"]["total"]
         bot.send_message(549143999814959124, "other", tts = false, embed = nil)
-      else
-        bot.send_message(549143999814959124, "QT", tts = false, embed = nil)
+      elsif qt_count == 0
+        bot.send_message(549143999814959124, "/poll "クイック出場可能時間" "20:00" "20:30" "21:00" "Tier8希望" "Tier10希望" "未定" "参加不可"", tts = false, embed = nil)
+        qt_count = 1
       end
     end
   end
-  event.send_message("over")
 end
 
 bot.run
