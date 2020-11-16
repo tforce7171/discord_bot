137f63a25e403be92279929f021ebe2ee1ae0896
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2020-11-16 23:05:00 +0900

    Add files via upload

--- bot_test.rb
+++ bot_test.rb
@@ -10,16 +10,25 @@ prefix:'/',
 )
 
 bot.command :ola do |event|
+
+  event.send_message("starting")
+
   url = "https://api.wotblitz.asia/wotb/tournaments/list/?application_id=eda85c3d6ddbb56920d3544319a4a788&fields=start_at%2Ctitle%2Ctournament_id"
   client = HTTPClient.new
   response = client.get(url)
   results = JSON.parse(response.body)
 
+  event.send_message("api parsed")
+  if resulta["data"]
+    event.send_message("api retrieved")
+  end
+  
   results["data"].each do |result|
     today_unix = Date.today.to_time.to_i
     start_at_date_unix = result["start_at"]-72000
     tournament_id = result["tournament_id"]
     if today_unix == start_at_date_unix
+      event.send_message("date match found")
       url = "https://api.wotblitz.asia/wotb/tournaments/stages/?application_id=eda85c3d6ddbb56920d3544319a4a788&tournament_id=#{tournament_id}"
       client = HTTPClient.new
       response = client.get(url)
