a15da074b2719e547d1b6e3729c6d008d1861465
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2020-11-18 18:21:28 +0900

    Add files via upload

--- bot_test.rb
+++ bot_test.rb
@@ -15,13 +15,13 @@ exec_count = 0
 application_id = ENV['APPLICATION_ID']
 
 bot.heartbeat do |event|
-  # bot.send_message(549145858105540609, "tick")
+  # bot.send_message(764701181783572500, "tick")
   now_hour = Time.now.hour
   now_min = Time.now.min
 
   if assign_time_hour == now_hour && assign_time_min <= now_min && exec_count == 0
 
-    # bot.send_message(549145858105540609,"starting")
+    # bot.send_message(764701181783572500,"starting")
 
     url = "https://api.wotblitz.asia/wotb/tournaments/list/?application_id=#{application_id}&fields=start_at%2Ctitle%2Ctournament_id"
     client = HTTPClient.new
@@ -29,9 +29,9 @@ bot.heartbeat do |event|
     results = JSON.parse(response.body)
     qt_count = 0
 
-    # bot.send_message(549145858105540609,"api parsed")
+    # bot.send_message(764701181783572500,"api parsed")
     # if results["data"]
-    #   bot.send_message(549145858105540609,"api retrieved")
+    #   bot.send_message(764701181783572500,"api retrieved")
     # end
 
     results["data"].each do |result|
@@ -39,11 +39,11 @@ bot.heartbeat do |event|
       start_at_date_unix = Time.at(result["start_at"]).to_date.to_time.to_i
       tournament_id = result["tournament_id"]
 
-      # bot.send_message(549145858105540609,"#{today_unix};#{start_at_date_unix};#{tournament_id}")
+      # bot.send_message(764701181783572500,"#{today_unix};#{start_at_date_unix};#{tournament_id}")
 
       if today_unix == start_at_date_unix
 
-        # bot.send_message(549145858105540609,"date match found")
+        # bot.send_message(764701181783572500,"date match found")
 
         url = "https://api.wotblitz.asia/wotb/tournaments/stages/?application_id=#{application_id}&tournament_id=#{tournament_id}"
         client = HTTPClient.new
@@ -52,21 +52,21 @@ bot.heartbeat do |event|
 
         if result["meta"]["total"]
           title = result["data"]["title"]
-          bot.send_message(549143999814959124, "#{title}")
+          bot.send_message(764701181783572500, %Q[/poll "#{title}" "19:00" "19:30" "19:55" "未定" "参加不可"])
         elsif qt_count == 0
-          bot.send_message(549143999814959124, %Q[/poll "クイック出場可能時間" "20:00" "20:30" "21:00" "Tier8希望" "Tier10希望" "未定" "参加不可"])
+          bot.send_message(764701181783572500, "?qt8")
           qt_count = 1
         end
       end
     end
 
     exec_count = 1
-    # bot.send_message(549145858105540609,"over")
+    # bot.send_message(764701181783572500,"over")
   end
 
   if assign_time_hour < now_hour && exec_count == 1
     exec_count = 0
-    # bot.send_message(549145858105540609,"exec_count reset")
+    # bot.send_message(764701181783572500,"exec_count reset")
   end
 end
 bot.run
